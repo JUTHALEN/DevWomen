38b9abebc6b9e8ea8361b4972681428b354f3ed8
jalendem <judith.alende@gmail.com>
2023-03-20 13:48:21 +0100

    Estado inicial del proyecto

---
+++ src/main/java/com/proyecto/DevWomenApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWomenApplication.class, args);
+	}
+
+}
